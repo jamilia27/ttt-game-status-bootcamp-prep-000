0b559e5c997244d3f9ed352d8c6271035f8d2b78
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-27 16:16:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,5 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.any? do |win_combo|
     board[win_combo[0]] == "X" && board[win_combo[1]] =="X" && board[win_combo[2]] == "X" || board[win_combo[0]] == "O" && board[win_combo[1]] =="O" && board[win_combo[2]] == "O"
-    else
-      return FALSE
-    end
   end
 end
